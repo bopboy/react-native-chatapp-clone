6794d755631cc11c3f271770e4a4715937766fdf
bopboy2 <scp0000@gmail.com>
2021-10-30 00:10:02 +0900

    회원가입 기능구현 (공백문자 처리 부분 오류 있는 듯)

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,37 @@
 import { initializeApp } from 'firebase/app'
 import config from '../firebase.json'
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 
 const app = initializeApp(config)
-
 const auth = getAuth()
+const storage = getStorage(app)
+
 export const signin = async ({ email, password }) => {
     const { user } = await signInWithEmailAndPassword(auth, email, password)
+    return user
+}
+
+const uploadImage = async uri => {
+    if (uri.startsWith('https')) return uri
+    const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+        xhr.onload = function () { resolve(xhr.response) }
+        xhr.onerror = function () { reject(new TypeError('Network request failed')) }
+        xhr.responseType = 'blob'
+        xhr.open('GET', uri, true)
+        xhr.send(null)
+    })
+    const user = auth.currentUser
+    const imageRef = ref(storage, `/profile/${user.uid}/photo.png`)
+    const snapshot = await uploadBytes(imageRef, blob, { contentType: 'image/png' })
+    console.log(snapshot)
+    blob.close()
+    return await getDownloadURL(snapshot.ref)
+}
+export const signup = async ({ name, email, password, photo }) => {
+    const { user } = await createUserWithEmailAndPassword(auth, email, password)
+    const photoURL = await uploadImage(photo)
+    await updateProfile(user, { displayName: name, photoURL })
     return user
 }
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,7 +1,10 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import styled from 'styled-components'
-import { Button, Image, Input } from '../components'
+import { Button, Image, Input, ErrorMessage } from '../components'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { signup } from '../firebase'
+import { Alert } from 'react-native'
+import { validateEmail, removeWhitespace } from '../utils'
 
 const Container = styled.View`
     flex:1;
@@ -11,7 +14,7 @@ const Container = styled.View`
     padding:50px 20px;
 `
 const DEFAULT_PHOTO = 'https://firebasestorage.googleapis.com/v0/b/rn-chat-app-7368c.appspot.com/o/face.png?alt=media'
-const Signup = () => {
+const Signup = ({ navigation }) => {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
@@ -20,28 +23,58 @@ const Signup = () => {
     const refPassword = useRef(null)
     const refPasswordConfirm = useRef(null)
     const [photo, setPhoto] = useState(DEFAULT_PHOTO)
-    const _handleSignupBtnPress = () => {
-        console.log('singup')
+    const [errorMessage, setErrorMessage] = useState("")
+    const [disabled, setDisabled] = useState(true)
+    const refDidMount = useRef(null)
+    const _handleSignupBtnPress = async () => {
+        try {
+            const user = await signup({ name, email, password, photo })
+            navigation.navigate('Profile', { user })
+        } catch (e) {
+            Alert.alert('Sign up Error', e.message)
+        }
     }
+    useEffect(() => {
+        setDisabled(!(name && email && password && passwordConfirm && !errorMessage))
+    }, [email, name, passwordConfirm, password, errorMessage])
+    useEffect(() => {
+        if (refDidMount.current) {
+            let error = ''
+            if (!name) error = '이름을 입력해'
+            else if (!email) error = '이메일을 입력해'
+            else if (!validateEmail(email)) error = '제대로된 이메일 입력해'
+            else if (password.length < 6) error = '비번은 6자 이상이어야 해'
+            else if (password !== passwordConfirm) error = '비번은 같아야 해'
+            else error = ''
+            setErrorMessage(error)
+        } else {
+            refDidMount.current = true
+        }
+    }, [email, name, passwordConfirm, password])
     return (
         <KeyboardAwareScrollView extraScrollHeight={20}>
             <Container>
                 <Image showButton={true} url={photo} onChangePhoto={setPhoto} />
                 < Input label="Name" placeholder="Name" returnKeyType="next" value={name} onChangeText={setName}
                     onSubmitEditing={() => refEmail.current.focus()}
+                    onBlur={() => setName(name.trim())} maxLength={12}
                 />
                 <Input ref={refEmail} label="Email" placeholder="Email" returnKeyType="next" value={email} onChangeText={setEmail}
                     onSubmitEditing={() => refPassword.current.focus()}
+                // onBlur={() => setEmail(removeWhitespace(email))}
                 />
                 <Input ref={refPassword}
                     label="Password" placeholder="Password" returnKeyType="next" value={password} onChangeText={setPassword}
                     isPassword={true} onSubmitEditing={() => refPasswordConfirm.current.focus()}
+                // onBlur={() => setPassword(removeWhitespace(password))}
                 />
                 <Input ref={refPasswordConfirm}
                     label="PasswordConfirm" placeholder="PasswordConfirm" returnKeyType="done" value={passwordConfirm} onChangeText={setPasswordConfirm}
                     isPassword={true} onSubmitEditing={_handleSignupBtnPress}
+                // onBlur={() => setPasswordConfirm(removeWhitespace(passwordConfirm))}
                 />
-                <Button title="Sign up" onPress={_handleSignupBtnPress} />
+                <ErrorMessage message={errorMessage} />
+                <Button title="Sign up" onPress={_handleSignupBtnPress} disabled={disabled} />
             </Container >
         </KeyboardAwareScrollView>
     )
